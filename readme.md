7a940976251b6c656ba2615536539dacac192785
Alexandre BOURBEILLON <alexandrebourbeillon@gmail.com>
2024-10-14 13:16:11 +0200

    Initial commit with current version of RD

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Jasmin Postyping analysis
+
+This repo objective is to provide a set of semantic analysis comming after Jasmin typing step.
+Currently, it contains: 
+- Reaching definitions analysis
+
+
+# install 
+
+This repo use nix as its package manager. To install dependencies, just run:
+```
+nix-shell
+``` 
+
+For more informations, check [nix](https://nixos.org/)
+
---
+++ src/analysis.ml
@@ -0,0 +1,33 @@
+
+open Jasmin
+open Rd_structured
+
+module Arch =
+  (val let use_set0 = true and use_lea = false in
+       let call_conv = Glob_options.Linux in
+       let module C : Arch_full.Core_arch =
+         (val CoreArchFactory.core_arch_x86 ~use_lea ~use_set0 call_conv)
+       in
+       (module Arch_full.Arch_from_Core_arch (C) : Arch_full.Arch))
+
+
+  
+let () = 
+  try 
+    Sys.argv.(1) 
+    |> Pretyping.tt_file Arch.arch_info Pretyping.Env.empty None None
+    |> fst 
+    |> Pretyping.Env.decls 
+    |> Compile.preprocess Arch.reg_size Arch.asmOp 
+    |> rd_prog
+    |> ignore
+  with 
+  | Pretyping.TyError (loc, e) ->
+      Format.eprintf "%a: %a@." Location.pp_loc loc Pretyping.pp_tyerror e;
+      exit 2
+  | Syntax.ParseError (loc, None) ->
+      Format.eprintf "Parse error: %a@." Location.pp_loc loc;
+      exit 3
+  | Invalid_argument _ ->
+      Format.eprintf "usage: %s FILE" Sys.argv.(0);
+      exit 4
