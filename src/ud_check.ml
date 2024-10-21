759d45e21791a01d23bc34febd33a3acb886422e
Alexandre BOURBEILLON <alexandrebourbeillon@gmail.com>
2024-10-21 17:28:00 +0200

    adding located variable set

--- src/ud_check.ml
+++ src/ud_check.ml
@@ -5,6 +5,16 @@ open Prog
 open Decoratum
 
 
+module LV = struct
+  type t = (int) gvar_i
+  let compare x y =
+    let ux:CoreIdent.uid = (L.unloc x).v_id in 
+    let uy:CoreIdent.uid = (L.unloc y).v_id in    
+    Uint63.compares ux uy
+
+end 
+
+module Slv = Set.Make(LV)
 
 type ud_error = 
 | VarNotIntialized of ((int) gvar_i)
@@ -23,10 +33,42 @@ let uderror ~loc (code:ud_error) =
 let rs_uderror ~loc (code:ud_error) = 
   raise (uderror ~loc code)
 
+let is_local (v:int ggvar) = 
+  match v.gs with 
+  | Slocal -> true
+  | _ -> false
+let rec lvars_e (e:expr) : Slv.t =
+  match e with
+  | Pconst _ -> Slv.empty
+  | Pbool  _ -> Slv.empty
+  | Parr_init _ -> Slv.empty
+  | Pvar var -> 
+    if is_local var 
+    then Slv.singleton var.gv
+    else Slv.empty
+  | Pget  (_ , _ , _,var,expr) -> 
+    let eset = lvars_e expr in
+    if is_local var then Slv.add var.gv eset
+    else 
+      eset
+  | Psub (_,_,_,var,expr) ->
+    let eset = lvars_e expr in
+    if is_local var then Slv.add var.gv eset
+    else 
+      eset
+  | Pload (_,_,var,expr) -> 
+    let eset = lvars_e expr in
+    Slv.add var eset (*this var is always local*)
+  | Papp1 (_,expr) -> (lvars_e expr) 
+  | Papp2 (_,l,r) -> Slv.union (lvars_e l) (lvars_e r)
+  | PappN (_,exprs) -> List.fold (fun sl e -> Slv.union sl (lvars_e e)) Slv.empty exprs
+  | Pif (_,e1,e2,e3) -> Slv.union (lvars_e e1) (Slv.union (lvars_e e2) (lvars_e e3))
 
-let check_ud_var (dom:Domain.t) (_:L.i_loc) (modified_local_var: var): unit =
-  match Mv.find_opt modified_local_var dom with 
-  | None -> assert false (*TODO : handle error here*)
+
+let check_ud_var (dom:Domain.t) (_:L.i_loc) (modified_local_var: 'len gvar_i): unit =
+  match Mv.find_opt (L.unloc modified_local_var) dom with 
+  | None -> 
+    rs_uderror ~loc:(L.loc modified_local_var) (VarNotIntialized modified_local_var)
   | Some iset -> 
     if Si.is_empty iset then (
       assert false (*TODO : handle error here*)
@@ -34,8 +76,9 @@ let check_ud_var (dom:Domain.t) (_:L.i_loc) (modified_local_var: var): unit =
 
 
 let ud_expr (dom:Domain.t) (instr:L.i_loc) (locvars:Sv.t) (e: expr) : unit =
-  let used_locals_var: Sv.t= Sv.inter (Prog.vars_e e) (locvars) in
-  Sv.iter (check_ud_var dom instr) used_locals_var
+  let found_vars = lvars_e e in
+  let used_local_vars = Slv.fold (fun e sl -> if (Sv.mem (L.unloc e) locvars) then (Slv.add e sl) else sl) found_vars Slv.empty in
+  Slv.iter (check_ud_var dom instr) used_local_vars
 
 let ud_range (dom:Domain.t)  (locvars: Sv.t) (instr:L.i_loc) ((_,e1,e2): 'len grange) : unit =
   ud_expr dom instr locvars e1;
