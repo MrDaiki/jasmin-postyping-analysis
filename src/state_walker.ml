7605d03f6b73ac92314d30e9ea7dff10347b2ba0
Alexandre BOURBEILLON <alexandrebourbeillon@gmail.com>
2024-11-13 18:50:51 +0100

    returning state_walker to a former usable state

--- src/state_walker.ml
+++ src/state_walker.ml
@@ -1,27 +1,26 @@
 open Jasmin
 open Prog
-open Wsize
 
 module type SimpleMutator = sig
   type state
 
   val loop_stop_condition : state -> state -> bool
 
-  val cmerge : L.i_loc -> state -> state -> state
+  val merge : L.i_loc -> state -> state -> state
 
-  val cassume : L.i_loc -> int gexpr -> state -> state
+  val cond : L.i_loc -> int gexpr -> state -> state
 
-  val cloop : int gexpr -> L.i_loc -> state -> state -> state
+  val cif : L.i_loc -> int gexpr -> state -> state * state
 
-  val cassign : int glval -> E.assgn_tag -> int gty -> int gexpr -> L.i_loc -> state -> state
+  val cassign : L.i_loc -> int glval -> E.assgn_tag -> int gty -> int gexpr -> state -> state
 
-  val fcall : int glvals -> funname -> int gexprs -> L.i_loc -> state -> state
+  val fcall : L.i_loc -> int glvals -> funname -> int gexprs -> state -> state
 
   val syscall :
-    int glvals -> BinNums.positive Syscall_t.syscall_t -> int gexprs -> L.i_loc -> state -> state
+    L.i_loc -> int glvals -> BinNums.positive Syscall_t.syscall_t -> int gexprs -> state -> state
 
   val copn :
-    int glval list -> E.assgn_tag -> 'asm Sopn.sopn -> int gexprs -> L.i_loc -> state -> state
+    L.i_loc -> int glval list -> E.assgn_tag -> 'asm Sopn.sopn -> int gexprs -> state -> state
 end
 
 module SimpleWalker (Mutator : SimpleMutator) = struct
@@ -44,24 +43,21 @@ module SimpleWalker (Mutator : SimpleMutator) = struct
       (cmp_expr, incr_expr)
 
   let walk_assign lv tag gty expr loc state =
-      let state = Mutator.cassign lv tag gty expr loc state in
+      let state = Mutator.cassign loc lv tag gty expr state in
       (Cassgn (lv, tag, gty, expr), state)
 
   let rec walk_instr (state : Mutator.state) (instr : ('info, 'asm) instr) :
       Mutator.state * (int, Mutator.state, 'asm) ginstr =
       let out, state = walk_instr_r instr.i_desc instr.i_loc state in
-      (state, {instr with i_desc= out; i_info= state})
-
-  and walk_loop_body stmt1 stmt2 state =
-      let state, stmt2 = walk_stmt stmt2 state in
-      let state, stmt1 = walk_stmt stmt1 state in
-      (stmt1, stmt2, state)
+      (state, {i_desc= out; i_loc= instr.i_loc; i_info= state; i_annot= instr.i_annot})
 
   and walk_while al body1 e body2 loc prev =
       let state, _ = walk_stmt body1 prev in
+      let state = Mutator.cond loc e state in
       let rec loop prev =
-          let body1, body2, state = walk_loop_body body1 body2 prev in
-          let state = Mutator.cloop e loc prev state in
+          let state, body2 = walk_stmt body2 prev in
+          let state, body1 = walk_stmt body1 state in
+          let state = Mutator.cond loc e state in
           if Mutator.loop_stop_condition prev state then
             (Cwhile (al, body1, e, body2), state)
           else
@@ -76,13 +72,13 @@ module SimpleWalker (Mutator : SimpleMutator) = struct
           | E.DownTo -> walk_assign (Lvar x) AT_none (L.unloc x).v_ty gend loc prev
       in
       let cmp_exp, incr_exp = build_assign_from_for x direction gstart gend in
+      let state = Mutator.cond loc cmp_exp state in
       let rec loop prev =
-          let _, body2, state = walk_loop_body [] body prev in
+          let state, body = walk_stmt body prev in
           let _, state = walk_assign (Lvar x) AT_none (L.unloc x).v_ty incr_exp loc state in
-          let state = Mutator.cassume loc cmp_exp state in
-          let state = Mutator.cmerge loc prev state in
+          let state = Mutator.cond loc cmp_exp state in
           if Mutator.loop_stop_condition prev state then
-            (Cfor (x, (direction, gstart, gend), body2), state)
+            (Cfor (x, (direction, gstart, gend), body), state)
           else
             loop state
       in
@@ -93,19 +89,19 @@ module SimpleWalker (Mutator : SimpleMutator) = struct
       match instr with
       | Cassgn (lv, tag, gty, expr) -> walk_assign lv tag gty expr loc state
       | Ccall (lvs, fname, args) ->
-          let state = Mutator.fcall lvs fname args loc state in
+          let state = Mutator.fcall loc lvs fname args state in
           (Ccall (lvs, fname, args), state)
       | Csyscall (lvs, syscall, exprs) ->
-          let state = Mutator.syscall lvs syscall exprs loc state in
+          let state = Mutator.syscall loc lvs syscall exprs state in
           (Csyscall (lvs, syscall, exprs), state)
       | Copn (lvs, tag, sopn, exprs) ->
-          let state = Mutator.copn lvs tag sopn exprs loc state in
+          let state = Mutator.copn loc lvs tag sopn exprs state in
           (Copn (lvs, tag, sopn, exprs), state)
       | Cif (e, th, el) ->
-          let state = Mutator.cassume loc e state in
-          let s1, th = walk_stmt th state in
-          let s2, el = walk_stmt el state in
-          let state = Mutator.cmerge loc s1 s2 in
+          let s1, s2 = Mutator.cif loc e state in
+          let s1, th = walk_stmt th s1 in
+          let s2, el = walk_stmt el s2 in
+          let state = Mutator.merge loc s1 s2 in
           (Cif (e, th, el), state)
       | Cfor (x, gr, body) -> walk_for x gr body loc state
       | Cwhile (al, body1, e, body2) -> walk_while al body1 e body2 loc state
@@ -130,80 +126,50 @@ module SimpleWalker (Mutator : SimpleMutator) = struct
 end
 
 (*
-   module ExampleMutator : SimpleMutator = struct
-       type state = unit
+module MemoryEffectMutator : SimpleMutator with type state = memory_effect = struct
+  type state = memory_effect
 
-       type constant = unit
-       (* function name -> mutable pointer variables and their position in arg list *)
+  let loop_stop_condition (prev : state) (state : state) : bool = true
 
-       let loop_stop_condition _ _ _ = true
+  let merge (loc : L.i_loc) (state : state) (state : state) : state = state
 
-       let cif (const : constant) (expr : int gexpr) (loc : L.i_loc) (th_state : state) (el_state : state)
-           : state =
-           th_state
+  let cond (loc : L.i_loc) (cond : int gexpr) (state : state) : state = state
 
-       let cfor
-           (const : constant)
-           (loop_var : int gvar_i)
-           (range : int grange)
-           (loc : L.i_loc)
-           (prev : state)
-           (after : state) : state =
-           after
+  let cif (loc : L.i_loc) (cond : int gexpr) (state : state) : state * state = (state, state)
 
-       let cfor_decl
-           (const : constant)
-           (loop_var : int gvar_i)
-           (range : int grange)
-           (loc : L.i_loc)
-           (state : state) : state =
-           state
+  let cassign
+      (loc : L.i_loc)
+      (lv : int glval)
+      (tag : E.assgn_tag)
+      (ty : int gty)
+      (expr : int gexpr)
+      (state : state) : state =
+      state
 
-       let cwhile
-           (const : constant)
-           (align : E.align)
-           (condition : int gexpr)
-           (loc : L.i_loc)
-           (b1_state : state)
-           (b2_state : state) : state =
-           b2_state
+  let fcall
+      (loc : L.i_loc)
+      (lvs : int glvals)
+      (funname : funname)
+      (exprs : int gexprs)
+      (state : state) : state =
+      state
 
-       let cassign
-           (const : constant)
-           (lv : int glval)
-           (tag : E.assgn_tag)
-           (ty : int gty)
-           (expr : int gexpr)
-           (loc : L.i_loc)
-           (state : state) : state =
-           state
+  let syscall
+      (loc : L.i_loc)
+      (lvs : int glvals)
+      (syscall : BinNums.positive Syscall_t.syscall_t)
+      (exprs : int gexprs)
+      (state : state) : state =
+      state
 
-       let fcall
-           (const : constant)
-           (lv : int glvals)
-           (fname : funname)
-           (args : int gexprs)
-           (loc : L.i_loc)
-           (state : state) : state =
-           state
+  let copn
+      (loc : L.i_loc)
+      (lvs : int glvals)
+      (tag : E.assgn_tag)
+      (op : 'asm Sopn.sopn)
+      (exprs : int gexprs)
+      (state : state) : state =
+      state
+end
 
-       let syscall
-           (const : constant)
-           (lv : int glvals)
-           (syscall : BinNums.positive Syscall_t.syscall_t)
-           (args : int gexprs)
-           (loc : L.i_loc)
-           (state : state) : state =
-           state
-
-       let copn
-           (const : constant)
-           (lvs : int glval list)
-           (tag : E.assgn_tag)
-           (opn : 'asm Sopn.sopn)
-           (args : int gexprs)
-           (loc : L.i_loc)
-           (state : state) : state =
-           state
-     end
 *)
