7dab71922fdf04f042eba2592fddb4723781ce86
Alexandre BOURBEILLON <alexandrebourbeillon@gmail.com>
2024-12-10 15:00:28 +0100

    changing expression walker to make it a functor (non top level)

--- src/expression_walker.ml
+++ src/expression_walker.ml
@@ -27,26 +27,34 @@ module type ExpressionState = sig
   val pif : 'len gty -> t -> t -> t -> t
 end
 
-module ExpressionWalker (State : ExpressionState) = struct
-  type state = State.t
+module ExpressionWalker = struct
+  module type S = sig
+    type state
 
-  let rec walk_expr (expr : int gexpr) : state =
-      match expr with
-      | Pconst c -> State.pconst c
-      | Pbool b -> State.pbool b
-      | Parr_init ws -> State.parr_init ws
-      | Pvar var -> State.pvar var
-      | Pget (al, acc, ws, var, expr) ->
-          let state = walk_expr expr in
-          State.pget al acc ws var state
-      | Psub (al, ws, i, var, expr) ->
-          let state = walk_expr expr in
-          State.psub al ws i var state
-      | Pload (al, ws, var, expr) ->
-          let state = walk_expr expr in
-          State.pload al ws var state
-      | Papp1 (op, expr) -> State.app1 op (walk_expr expr)
-      | Papp2 (op, l, r) -> State.app2 op (walk_expr l) (walk_expr r)
-      | PappN (opn, exprs) -> State.appN opn (List.map walk_expr exprs)
-      | Pif (ty, e1, e2, e3) -> State.pif ty (walk_expr e1) (walk_expr e2) (walk_expr e3)
+    val walk_expr : int gexpr -> state
+  end
+
+  module Make (State : ExpressionState) : S with type state = State.t = struct
+    type state = State.t
+
+    let rec walk_expr (expr : int gexpr) : state =
+        match expr with
+        | Pconst c -> State.pconst c
+        | Pbool b -> State.pbool b
+        | Parr_init ws -> State.parr_init ws
+        | Pvar var -> State.pvar var
+        | Pget (al, acc, ws, var, expr) ->
+            let state = walk_expr expr in
+            State.pget al acc ws var state
+        | Psub (al, ws, i, var, expr) ->
+            let state = walk_expr expr in
+            State.psub al ws i var state
+        | Pload (al, ws, var, expr) ->
+            let state = walk_expr expr in
+            State.pload al ws var state
+        | Papp1 (op, expr) -> State.app1 op (walk_expr expr)
+        | Papp2 (op, l, r) -> State.app2 op (walk_expr l) (walk_expr r)
+        | PappN (opn, exprs) -> State.appN opn (List.map walk_expr exprs)
+        | Pif (ty, e1, e2, e3) -> State.pif ty (walk_expr e1) (walk_expr e2) (walk_expr e3)
+  end
 end
